51c57a4b416c74e1730e5513fb7d7e9147bc8bd7
LazyMechanic <asharnrus@gmail.com>
2022-04-17 13:03:56 +0300

    Update docs

--- moq/src/lib.rs
+++ moq/src/lib.rs
@@ -172,17 +172,60 @@
 //! Syntax is like anonymous function:
 //! ```
 //! # fn main() {
+//! #[derive(Debug, PartialEq)]
+//! struct StructNamed {
+//!    field: i32,
+//! }
+//!
+//! impl StructNamed {
+//! fn get_field(&self) -> i32 {
+//!        self.field
+//!    }
+//! }
+//!
+//! struct StructUnnamed(i32);
+//!
+//! let arg1 = 1i32;
+//! let arg2 = 2i32;
+//! let arg3 = StructNamed { field: 3i32 };
+//! let arg4 = StructNamed { field: 4i32 };
+//! let arg5 = StructNamed { field: 5i32 };
+//! let arg6 = StructNamed { field: 6i32 };
+//! let arg7 = StructUnnamed(7i32);
+//!
 //! moq::lambda!(
+//! // Environment capturing (optional)
+//! //
+//! //      expression          var alias expression type
+//! //      |                   |         |
+//! //      vvvvvvvvvvvvvvvv    vvvvvv    vvvvvvvvvvv
+//!     [
+//!         arg1                       => i32,
+//!         arg2             as field0 => i32,
+//!         arg3                       => StructNamed,
+//!         arg4.field                 => i32,
+//!         arg5.field       as field2 => i32,
+//!         arg6.get_field() as field3 => i32,
+//!         arg7.0           as field4 => i32
+//!     ]
+//!
+//! // Function signature
+//! //  
 //! //  asyncness
-//! //  |        
 //! //  |        generic params (lifetimes)
-//! //  |        |
-//! //  |        |    arguments
-//! //  |        |    |
-//! //  |        |    |             return type
-//! //  |        |    |             |
-//! //  vvvvv    vvvv vvvvvvvvvvvv  vvvvvvvvvvvvvvv
-//!     async fn <'a>(arg: &'a str) -> &'static str {
+//! //  |        |    object that captured env, needs only if env specified
+//! //  |        |    |      arguments
+//! //  |        |    |      |                return type
+//! //  |        |    |      |                |
+//! //  vvvvv    vvvv vvvvv  vvvvvvvvvvvv     vvvvvvvvvvvv
+//!     async fn <'a>(&self, arg: &'a str) -> &'static str {
+//!         assert_eq!(self.arg1, 1);
+//!         assert_eq!(self.field0, 2);
+//!         assert_eq!(self.arg3, StructNamed { field: 3 });
+//!         assert_eq!(self.field, 4);
+//!         assert_eq!(self.field2, 5);
+//!         assert_eq!(self.field3, 6);
+//!         assert_eq!(self.field4, 7);
 //!         // ...
 //!         # unimplemented!()
 //!     }
@@ -194,14 +237,56 @@
 //! It turns to something like that:
 //! ```
 //! # fn main() {
+//! # struct StructNamed {
+//! #    field: i32,
+//! # }
+//! # impl StructNamed {
+//! # fn get_field(&self) -> i32 {
+//! #        self.field
+//! #    }
+//! # }
+//! # struct StructUnnamed(i32);
+//! # let arg1 = 1i32;
+//! # let arg2 = 2i32;
+//! # let arg3 = StructNamed { field: 3i32 };
+//! # let arg4 = StructNamed { field: 4i32 };
+//! # let arg5 = StructNamed { field: 5i32 };
+//! # let arg6 = StructNamed { field: 6i32 };
+//! # let arg7 = StructUnnamed(7i32);
 //! {
-//!     struct __MoqLambda {}
+//!     struct __MoqLambda {
+//!         arg1: i32,
+//!         field0: i32,
+//!         arg3: StructNamed,
+//!         field: i32,
+//!         field2: i32,
+//!         field3: i32,
+//!         field4: i32,
+//!     }
 //!     impl __MoqLambda {
-//!         fn new() -> Self { Self{} }
+//!         fn new(
+//!             arg1: i32,
+//!             field0: i32,
+//!             arg3: StructNamed,
+//!             field: i32,
+//!             field2: i32,
+//!             field3: i32,
+//!             field4: i32,
+//!         ) -> Self {
+//!             Self {
+//!                 arg1,
+//!                 field0,
+//!                 arg3,
+//!                 field,
+//!                 field2,
+//!                 field3,
+//!                 field4,
+//!             }
+//!         }
 //!     }
 //!     #[::moq::async_trait]
 //!     impl<'a> ::moq::AsyncFunc<(&'a str,), &'static str> for __MoqLambda {
-//!         async fn call<'__moq>(&'__moq self, args: (&'a str,)) -> &'static str
+//!         async fn call<'__moq>(&'__moq self, (arg,): (&'a str,)) -> &'static str
 //!         where (&'a str,): '__moq,
 //!               &'static str: '__moq
 //!         {
@@ -209,7 +294,15 @@
 //!             # unimplemented!()
 //!         }
 //!     }
-//!     __MoqLambda::new()
+//!     __MoqLambda::new(
+//!         arg1,             
+//!         arg2,             
+//!         arg3,             
+//!         arg4.field,       
+//!         arg5.field,       
+//!         arg6.get_field(),
+//!         arg7.0,           
+//!     )
 //! }
 //! # ;
 //! # }
