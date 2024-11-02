9763f1ca52a5ed02fd18e24449ad62255943f13e
LazyMechanic <asharnrus@gmail.com>
2024-11-02 16:01:16 +0300

    Add tests

--- test_suite/tests/func_generics.rs
+++ test_suite/tests/func_generics.rs
@@ -53,3 +53,51 @@ fn test4() {
     });
     assert_eq!(m.f::<_, &'static str>(1i32), "2");
 }
+
+#[test]
+fn test5() {
+    struct Dummy<'a, T> {
+        a: &'a str,
+        b: T,
+    }
+
+    #[moq::automock]
+    trait Trait {
+        fn f<'a, A>(&self, arg: Dummy<'a, A>)
+        where
+            A: 'static;
+    }
+
+    let m = TraitMock::new().expect_f(|arg: Dummy<i32>| {
+        assert_eq!(arg.a, "1");
+        assert_eq!(arg.b, 2);
+    });
+    let a = String::from("1");
+    m.f::<i32>(Dummy { a: &a, b: 2 });
+}
+
+#[test]
+fn test6() {
+    #[derive(Debug, Eq, PartialEq)]
+    struct Dummy<'a, T> {
+        a: &'a str,
+        b: T,
+    }
+
+    #[moq::automock]
+    trait Trait {
+        fn f<'a, A>(&self, arg: Dummy<'a, A>) -> Dummy<'a, A>
+        where
+            A: 'static;
+    }
+
+    let m = TraitMock::new().expect_f(moq::lambda!(
+        fn <'a>(arg: Dummy<'a, i32>) -> Dummy<'a, i32> {
+            assert_eq!(arg.a, "1");
+            assert_eq!(arg.b, 2);
+            Dummy { a: arg.a, b: 3 }
+        }
+    ));
+    let a = String::from("1");
+    assert_eq!(m.f::<i32>(Dummy { a: &a, b: 2 }), Dummy { a: "1", b: 3 });
+}
